db1870d0bb6922f0f3eb862a0daf094dcae477cb
Chingiz Sultanov <118419172+bnxwritecat@users.noreply.github.com>
2023-02-23 12:54:27 +0300

    Add files via upload

--- hom1.py
+++ hom1.py
@@ -27,3 +27,51 @@ a.names()
 a.hitpoints()
 print(a)
 print(len(a))
+
+
+#2
+class Dendroheroes(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly = False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+    
+    def hitpoints(self):
+        self.health_points = self.health_points ** 2
+        self.fly = True
+        print("HP:", self.health_points)
+    def phrase(self):
+        print(f"Hero flyes:{self.fly}")
+
+c = Dendroheroes("Alhaitam","Haravatat","Particular Field: Fetters of Phenomena",12409,"A process of elimination.", 1333)
+c.hitpoints()
+c.phrase()
+
+class AnemoHeroes(SuperHero):
+    def __init__(self, name, nickname, superpower, health_points, catchphrase, damage, fly = False):
+        super().__init__(name, nickname, superpower, health_points, catchphrase)
+        self.damage = damage
+        self.fly = fly
+    
+    def hitpoints(self):
+        self.health_points = self.health_points ** 2
+        self.fly = True
+        print("HP:", self.health_points)
+    def phrase(self):
+        print(f"Hero flyes:{self.fly}")
+    
+b = AnemoHeroes("Wanderer", "Scaramouche", "Kyougen: Five Ceremonial Plays", 9449, "You dare to gaze upon me!?", 1647)
+b.hitpoints()
+b.phrase()
+
+class Villain(AnemoHeroes):
+    people = "Monster"
+    def gen_x(self):
+        pass
+    
+    def crit_DMG(self):
+        crit = self.damage ** self.damage
+        print("Crit Damage:", crit)
+        
+d = Villain
+print(d.people)
